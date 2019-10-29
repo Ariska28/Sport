3f8ef672fcf393c7e950db18b1bc179a6c11ab09
Ariska28 <55794499+Ariska28@users.noreply.github.com>
2019-10-29 21:24:57 +0300

    all

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,31 @@ $().ready(function(){
     prevArrow: $('.slick__prewarrow'),
     nextArrow: $('.slick__nextarrow'),
     centerMode: true,
-    centerPadding: '50px',
+    centerPadding: '150px',
     slidesToShow: 3,
-    dots: true
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1500, 
+        settings: {
+          slidesToShow: 2,
+          dots: true,
+          centerPadding: '0px',
+          infinite:true,
+          centerMode: true,
+        }
+      },
+      {
+        breakpoint: 1000, 
+        settings: {
+          slidesToShow: 1,
+          dots: true,
+          centerPadding: '0px',
+          centerMode: true,
+          infinite:true,
+        }
+      }
+    ]
   });
 });
+
