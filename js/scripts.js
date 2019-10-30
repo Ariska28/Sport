a98bcc5639650f083c3a5b74b3136f344ccecdce
Ariska28 <55794499+Ariska28@users.noreply.github.com>
2019-10-30 10:43:20 +0300

    slick-block

--- js/scripts.js
+++ js/scripts.js
@@ -3,31 +3,8 @@ $().ready(function(){
     prevArrow: $('.slick__prewarrow'),
     nextArrow: $('.slick__nextarrow'),
     centerMode: true,
-    centerPadding: '150px',
+    centerPadding: '50px',
     slidesToShow: 3,
-    dots: true,
-    responsive: [
-      {
-        breakpoint: 1500, 
-        settings: {
-          slidesToShow: 2,
-          dots: true,
-          centerPadding: '0px',
-          infinite:true,
-          centerMode: true,
-        }
-      },
-      {
-        breakpoint: 1000, 
-        settings: {
-          slidesToShow: 1,
-          dots: true,
-          centerPadding: '0px',
-          centerMode: true,
-          infinite:true,
-        }
-      }
-    ]
+    dots: true
   });
 });
-
