5ceaba91c8ca0b239098883da17709a3dbd12d3f
Ariska28 <55794499+Ariska28@users.noreply.github.com>
2019-10-30 13:25:44 +0300

    efg

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,10 @@
-$().ready(function () {
+$().ready(function(){
   $('.slick__block').slick({
     prevArrow: $('.slick__prewarrow'),
     nextArrow: $('.slick__nextarrow'),
     centerMode: true,
-    centerPadding: '150px',
+    centerPadding: '50px',
     slidesToShow: 3,
-    dots: true,
-    responsive: [
-      {
-        breakpoint: 1000,
-        settings: {
-          slidesToShow: 1,
-          dots: true,
-          centerPadding: '0px',
-          centerMode: true,
-          infinite: true,
-        }
-      }
-    ]
+    dots: true
   });
 });
-
