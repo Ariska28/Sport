27820b944d0fe0023d7128098b7db7754859db5c
Ariska28 <55794499+Ariska28@users.noreply.github.com>
2019-10-31 20:56:34 +0300

    jhr

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,26 @@
-$().ready(function(){
-  $('.slick__block').slick({
-    prevArrow: $('.slick__prewarrow'),
-    nextArrow: $('.slick__nextarrow'),
-    centerMode: true,
-    centerPadding: '50px',
-    slidesToShow: 3,
-    dots: true
+$().ready(function () {
+  $().ready(function(){
+    $('.slick__block').slick({
+      prevArrow: $('.slick__prewarrow'),
+      nextArrow: $('.slick__nextarrow'),
+      centerMode: true,
+      centerPadding: '150px',
+      centerPadding: '50px',
+      slidesToShow: 3,
+      dots: true,
+      responsive: [
+        {
+          breakpoint: 1000,
+          settings: {
+            slidesToShow: 1,
+            dots: true,
+            centerPadding: '0px',
+            centerMode: true,
+            infinite: true,
+          }
+        }
+      ]
+    });
   });
-});
+  
+  });
